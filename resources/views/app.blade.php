9433c9ffa3a827dc6ff6293c250b1568e02273df
Leonardo Faria <leonardofaria@gmail.com>
2016-02-08 13:15:27 -0800

    Update UI

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Look! I'm CRUDding</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <title>Langara eSports</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class="container">
