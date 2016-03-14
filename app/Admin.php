8ad6b0ac6768f95aa9aebdef78a5d04e912ddbe0
Leonardo Faria <leonardofaria@gmail.com>
2016-03-13 21:11:58 -0700

    Add soft deletes

--- app/Admin.php
+++ app/Admin.php
@@ -3,10 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Model
 {
     protected $fillable = ['user_id'];
+
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
 
     public function user()
     {
